f67e7c32d168aa165b0a98c6fbe81c982ad6c5a3
UrstrulyBhavana <urstrulybhavana1432@gmail.com>
2024-04-19 12:01:40 +0530

    IIFE Concept

---
+++ main.js
@@ -0,0 +1,6 @@
+(function(num1, num2, num3=55) {
+    console.log(`Sum of three numbers ${num1 + num2 + num3}` );
+})(23, 44);
+
+//Write an IIFE in which you send two numbers as an argument. But in the parameter, it should receive three numbers, with the third one being a default parameter.
+// Inside the function, the console log the sum of three numbers.
